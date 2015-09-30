0e8b3b672e833809f4035657f606051541b41bca
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-09-30 23:53:21 +0200

    plot the data

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from pandas.tseries.offsets import Hour, Minute, Week, Month
+from pandas.tseries.offsets import Hour, Minute, Week, MonthBegin
 
 __author__ = 'Fredrik A. Madsen-Malmo'
+
+# Configure matplotlib
+font = {
+    'family': 'monospace',
+    'size': 10
+}
+
+# Apply settings
+plt.rc('font', **font)
 
 # Start importing files
 
@@ -27,8 +36,30 @@ def get_means(time_type, n):
     temp_timed = [temps.ix[x : x + time_type(1)] for x in last_times[1]]
 
     return [[[x.index[0] for x in load_timed], [x.mean() for x in load_timed]],
-            [[x.index[0] for x in temp_timed], [x.mean() for x in temp_timed]]
+            [[x.index[0] for x in temp_timed], [x.mean() for x in temp_timed]]]
 
 # Get the last times from each of the different "time-types"
-means = [get_means(time_type, n) for time_type, n in [[Minute, 20], [Hour, 10]]], [Week, 5], [MonthBegin, 4]]]
+means = [get_means(time_type, n) for time_type, n in [[Minute, 20], [Hour, 10], [Week, 5], [MonthBegin, 4]]]
 
+# For each time
+for mean in means:
+    # For load/temp inside of this
+    for type in mean:
+        # Create figure and axis-es
+        fig = plt.figure(); ax = fig.add_subplot(1,1,1)
+
+        # Add grid for easier viewing
+        ax.grid()
+
+        # Set a title
+        ax.set_title('name')
+
+        # Plot with dates on x axis
+        ax.plot(type[0], type[1], 'k-')
+
+        # Save the figure, implement naming system
+        plt.savefig('name.png', dpi=400, bbox_inches='tight')
+
+        # Cleanup
+        del fig
+        del ax
