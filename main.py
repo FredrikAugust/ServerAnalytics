ad925c9af9bbb6a6de3e96698f918ca2a9335019
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-09-30 22:24:33 +0200

    calculate mean

--- main.py
+++ main.py
@@ -17,17 +17,16 @@ loads, temps = [pd.read_csv(
                     '%s.csv' % x, header=None, index_col=0, parse_dates=True
                 ) for x in ['loads', 'temps']]
 
-# Get the last times
-# E.g.
-# We need to get the last 20 minutes to be able
-# to calculate the 20 last averages/minute
-
-def get_last_time(time_type, n):
-    '''Returns the `n` last `time_type` as a list before
-    the last entry of `item`.
+def get_means(time_type, n):
+    '''This function gets the `n` last `time_type` and calculates
+    the mean value of said selection.
     '''
+    last_times = [[y.index[-1] - Minute(x) for x in range(n)] for y in [loads, temps]]
 
-    return [[y.index[-1] - Minute(x) for x in range(n)] for y in [loads, temps]]
+    load_timed = [loads.ix[x:x+time_type(1)] for x in last_times[0]]
+    temp_timed = [temps.ix[x:x+time_type(1)] for x in last_times[1]]
+
+    return [[[x.index[0], x.mean()] for x in load_timed], [[x.index[0], x.mean()] for x in temp_timed]]
 
 # Get the last times from each of the different "time-types"
-last_times = [get_last_time(time_type, n) for time_type, n in [[Minute, 20], [Hour, 10], [Week, 5], [MonthBegin, 4]]]
+means = [get_means(time_type, n) for time_type, n in [[Minute, 20], [Hour, 10], [Week, 5], [MonthBegin, 4]]]
